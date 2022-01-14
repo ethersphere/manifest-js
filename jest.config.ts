a0b0605b99987010727e68963caa49fd378d4f36
Vojtech Simetka <vojtech@simetka.cz>
2022-01-14 11:41:54 +0100

    ci: bump to latest queen bee version

--- jest.config.ts
+++ jest.config.ts
@@ -1,34 +1,19 @@
-/* eslint-disable no-console */
 /*
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/en/configuration.html
  */
-import { BeeDebug } from '@ethersphere/bee-js'
 import type { Config } from '@jest/types'
 
 export default async (): Promise<Config.InitialOptions> => {
-  if (!process.env.BEE_POSTAGE) {
-    console.log('BEE_POSTAGE env variable is not set, creating postage stamp...')
-    try {
-      const beeDebugUrl = process.env.BEE_DEBUG_API_URL || 'http://localhost:1635'
-      const bee = new BeeDebug(beeDebugUrl)
-      process.env.BEE_POSTAGE = await bee.createPostageBatch('1', 20)
-      console.log('Queen stamp: ', process.env.BEE_POSTAGE)
-      // sleep for 11 seconds (10 blocks with ganache block time = 1s)
-      // needed for postage batches to become usable
-      // FIXME: sleep should be imported for this, but then we fail with
-      //        Could not find a declaration file for module 'tar-js'
-      await new Promise<void>(resolve => setTimeout(() => resolve(), 11_000))
-    } catch (e) {
-      // It is possible that for unit tests the Bee nodes does not run
-      // so we are only logging errors and not leaving them to propagate
-      console.error(e)
-    }
-  }
-
   return {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
+
+    // This will setup the prerequisites for the tests to run
+    globalSetup: './tests-setup.ts',
 
     // The directory where Jest should output its coverage files
     coverageDirectory: 'coverage',
@@ -41,9 +26,6 @@ export default async (): Promise<Config.InitialOptions> => {
 
     // The root directory that Jest should scan for tests and modules within
     rootDir: 'test',
-
-    testEnvironment: 'node',
-    testRegex: 'test/.*\\.spec\\.ts',
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
     testPathIgnorePatterns: ['/node_modules/'],
